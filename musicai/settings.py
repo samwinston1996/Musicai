4152de91ce5058748bca39f25b6cd2ddaec30fa3
muhammadabir64 <muhammadabir99@gmail.com>
2023-07-08 00:11:07 +0600

    first commit 6

--- musicai/settings.py
+++ musicai/settings.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
     'django.contrib.sites',
 
     'allauth',
